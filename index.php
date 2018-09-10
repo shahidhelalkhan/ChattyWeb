821e4a6719ec027ee60252b013133a18557a2a11
shahidhelalkhan <shahidhelalkhan@gmail.com>
2018-09-10 23:21:53 +0530

    Login Page designed
    Designed Responsive Login Page

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="main.js"></script>
+</head>
+<body>
+
+<div class="form">
+    <center>
+        <h1>Chatty</h1>
+<form action="login" method="post">
+<input type="text" name="username" id="uname" placeholder="Username" >
+<input type="text" name="password" id="passwd" placeholder="Password" ><br>
+<input type="checkbox" name="remember" id="rem"> Remeber me? <br>
+<input type="button" value="Submit" name="submit"><br>
+<input type="button" value="Register" name="register" style="background:#c31432;color:white;">
+</center>
+</form>
+</div>
+
+</body>
+</html>
